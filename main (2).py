3a562672e52272938fbb0837ef239fa5b7a32038
naveenpeterson <144438495+naveenpeterson@users.noreply.github.com>
2023-10-09 15:03:08 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,22 +1,21 @@
-# Define the base class player 
-class player:
-    def play(self):
-         print("The player is playing cricket.")
+# Python program to check if year is a leap year or not
 
-# Define the derived class Batsman
-class Batsman(player):
-    def play(self):
-         print("The batsman is batting.")
+year = 2000
 
-# Define the derived class Bowler
-class Bowler(player):
-    def play(self):
-        print("The bowler is bowling.")
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
 
-# Create objects of Batsman and Bowler classes 
-batsman = Batsman()
-bowler = Bowler()
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
